688f789d0c44aacbd9241251dcfa3309e7733b8f
OzRagwort <ozragwort@gmail.com>
2020-11-21 00:22:40 +0900

    플레이리스트 여러개 동시 업데이트 기능 추가

--- src/main/resources/static/js/app/playlistsUpdate.js
+++ src/main/resources/static/js/app/playlistsUpdate.js
@@ -4,8 +4,13 @@ var main = {
         $('#btn-playlist-update').on('click', function() {
             _this.playlist();
         });
+        $('#btn-playlist-update-multi').on('click', function() {
+            _this.multiple_playlists();
+        });
     },
     playlist : function() {
+        this.playlistDo($('#updatePlaylistId').val());
+
         var data = {
             channelId : $('#updatePlaylistId').val()
         };
@@ -21,6 +26,37 @@ var main = {
             window.location.href = '/admin/videos/crud';
         }).fail(function(error) {
             alert(JSON.stringify(error));
+        });
+    },
+    multiple_playlists : function() {
+        var playlistIds = $('#updateMultiPlaylistId').val();
+        var playlistIdsArray = playlistIds.split(',');
+        var failList = [];
+
+        for(var i = 0 ; i < playlistIdsArray.length ; i++) {
+            if ( this.playlistDo(playlistIdsArray[i]) ) {
+                failList.push(playlistIdsArray[i]);
+            }
+        }
+
+        $('#returnText').val(failList);
+
+    },
+    playlistDo : function(channelId) {
+        var data = {
+            channelId : channelId
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/moaon/v1/videos/uploadslist',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            return false;
+        }).fail(function(error) {
+            return true;
         });
     }
 };
