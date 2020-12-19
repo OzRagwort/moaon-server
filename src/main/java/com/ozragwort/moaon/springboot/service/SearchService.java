fbd2899ee8154aafa8126b661cb100da39ce2bd2
OzRagwort <ozragwort@gmail.com>
2020-12-19 17:28:10 +0900

    영상 검색 기능(lucene 검색 방법 수정)

--- src/main/java/com/ozragwort/moaon/springboot/service/SearchService.java
+++ src/main/java/com/ozragwort/moaon/springboot/service/SearchService.java
@@ -1,15 +1,14 @@
 package com.ozragwort.moaon.springboot.service;
 
 import com.ozragwort.moaon.springboot.domain.videos.Videos;
-import com.ozragwort.moaon.springboot.domain.videos.VideosRepository;
 import com.ozragwort.moaon.springboot.web.dto.VideosResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.apache.lucene.search.Query;
+
 import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.FullTextQuery;
 import org.hibernate.search.jpa.Search;
 import org.hibernate.search.query.dsl.QueryBuilder;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
@@ -28,24 +27,61 @@ public class SearchService {
     // 전체 인덱싱
     // com..component.StartupApplication.onApplicationEvent
 
-    // 영상 검색
     public List<VideosResponseDto> searchVideos(String keyword) {
+        return getVideos(keyword);
+    }
 
+    private List<VideosResponseDto> getVideos(String keyword) {
         FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
         QueryBuilder queryBuilder = fullTextEntityManager.getSearchFactory().buildQueryBuilder()
                 .forEntity(Videos.class)
                 .get();
-        Query query = queryBuilder.keyword().wildcard()
-                .onFields("videoName").boostedTo(2f)
-                .andField("videoDescription")
-                .matching("*" + keyword + "*")
-                .createQuery();
+
+        Query query = getWildcardQuery(queryBuilder, keyword);
+
         FullTextQuery fullTextQuery = fullTextEntityManager.createFullTextQuery(query, Videos.class);
         List<Videos> videosResponse = (List<Videos>) fullTextQuery.getResultList();
 
         return videosResponse.stream()
                 .map(VideosResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    // 딱 그 키워드와 같은 결과
+    private Query getKeywordQuery(QueryBuilder queryBuilder, String keyword) {
+        return queryBuilder.keyword()
+                .onFields("videoName").boostedTo(2f)
+                .andField("videoDescription")
+                .matching(keyword)
+                .createQuery();
+    }
+
+    // 조건 추가 (?, *)
+    // ? => 뭐가 들어가야 할 지 모르겠는것 test인지 text인지 모를때 te?t (한글자)
+    // * => ?가 여러글자
+    private Query getWildcardQuery(QueryBuilder queryBuilder, String keyword) {
+        return queryBuilder.keyword().wildcard()
+                .onFields("videoName").boostedTo(2f)
+                .andField("videoDescription")
+                .matching("*" + keyword + "*")
+                .createQuery();
+    }
+
+    // 유사한것들 찾기 (~, ~0.8) default = 0.5
+    private Query getFuzzyQuery(QueryBuilder queryBuilder, String keyword) {
+        return queryBuilder.keyword().fuzzy()
+                .onFields("videoName").boostedTo(2f)
+                .andField("videoDescription")
+                .matching(keyword)
+                .createQuery();
+    }
+
+    private Query getTestQuery(QueryBuilder queryBuilder, String keyword) {
+        return queryBuilder.keyword().fuzzy()
+                .onFields("videoName").boostedTo(2f)
+                .andField("videoDescription")
+                .matching(keyword)
+                .createQuery();
     }
 
 }
