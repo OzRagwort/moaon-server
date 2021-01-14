24f1283d3d531b0a375790488cc85c2777556d65
OzRagwort <ozragwort@gmail.com>
2021-01-15 05:52:08 +0900

    Change function api design of getting random data
    카테고리/채널 별 랜덤 채널이나 영상을 리턴하는 함수 api 설계 변경

--- src/main/java/com/ozragwort/moaon/springboot/web/ChannelsApiController.java
+++ src/main/java/com/ozragwort/moaon/springboot/web/ChannelsApiController.java
@@ -31,28 +31,21 @@ public class ChannelsApiController {
             @RequestParam(value = "no", required = false) Long idx,
             @RequestParam(value = "id", required = false) String channelId,
             @RequestParam(value = "category", required = false) Long categoryId,
-            @RequestParam(value = "maxResult", defaultValue = "10") int size,
+            @RequestParam(value = "randomCategory", required = false) Long randomCategoryId,
+            @RequestParam(value = "maxResults", defaultValue = "10") int size,
             @RequestParam(value = "page", defaultValue = "1") int pageCount
     ) {
-        if (idx != null)
+        if (idx != null) {
             return channelsService.findById(idx);
-        else if (channelId != null)
+        } else if (channelId != null) {
             return channelsService.findByChannelId(channelId);
-        else if (categoryId != null)
+        } else if (categoryId != null) {
             return channelsService.findByCategoryIdx(categoryId, PageRequest.of(pageCount - 1, size, Sort.by("idx").descending()));
-        else
+        } else if (randomCategoryId != null) {
+            return channelsService.findByCategoryIdxRand(randomCategoryId, size);
+        } else {
             return channelsService.findAll(PageRequest.of(pageCount - 1, size, Sort.by("idx").descending()));
-    }
-
-    @GetMapping("/channels/rand")
-    public List<ChannelsResponseDto> findRand(
-            @RequestParam(value = "category", required = false) Long categoryId,
-            @RequestParam(value = "count", defaultValue = "10") int count
-    ) {
-        if (categoryId != null)
-            return channelsService.findByCategoryIdxRand(categoryId, count);
-        else
-            return null;
+        }
     }
 
     @DeleteMapping("/channels/{idx}")
--- src/main/java/com/ozragwort/moaon/springboot/web/VideosApiController.java
+++ src/main/java/com/ozragwort/moaon/springboot/web/VideosApiController.java
@@ -40,48 +40,41 @@ public class VideosApiController {
             @RequestParam(value = "id", required = false) String videoId,
             @RequestParam(value = "channel", required = false) String channelId,
             @RequestParam(value = "category", required = false) Long categoryId,
-            @RequestParam(value = "maxResult", defaultValue = "10") int size,
+            @RequestParam(value = "randomChannel", required = false) String randomChannelId,
+            @RequestParam(value = "randomCategory", required = false) Long randomCategoryId,
+            @RequestParam(value = "maxResults", defaultValue = "10") int size,
             @RequestParam(value = "page", defaultValue = "1") int pageCount,
             @RequestParam(value = "sort", required = false) String sort,
             @RequestParam(value = "search", required = false) String keyword
     ) {
-        if (idx != null)
+        if (idx != null) {
             return videosService.findById(idx);
-        else if (videoId != null)
+        } else if (videoId != null) {
             return videosService.findByVideoId(videoId);
-        else if (channelId != null) {
-            if (sort == null)
+        } else if (channelId != null) {
+            if (sort == null) {
                 return videosService.findByChannelId(channelId, PageRequest.of(pageCount - 1, size, Sort.by("idx").descending()));
-            else if (sort.equals("asc"))
+            } else if (sort.equals("asc")) {
                 return videosService.findByChannelIdSort(channelId, PageRequest.of(pageCount - 1, size, Sort.by("videoPublishedDate").ascending()));
-            else if (sort.equals("desc"))
+            } else if (sort.equals("desc")) {
                 return videosService.findByChannelIdSort(channelId, PageRequest.of(pageCount - 1, size, Sort.by("videoPublishedDate").descending()));
-            else if (sort.equals("popular"))
+            } else if (sort.equals("popular")) {
                 return videosService.findByChannelIdSort(channelId, PageRequest.of(pageCount - 1, size, Sort.by("viewCount").descending()));
-            else
+            } else {
                 return videosService.findByChannelId(channelId, PageRequest.of(pageCount - 1, size, Sort.by("idx").descending()));
-            // sort 기능 추가 예정
+            }
         } else if (categoryId != null) {
             return videosService.findByCategoryIdx(categoryId, PageRequest.of(pageCount - 1, size, Sort.by("idx").descending()));
+        } else if (randomChannelId != null) {
+            return videosService.findByChannelIdRand(randomChannelId, size);
+        } else if (randomCategoryId != null) {
+            return videosService.findByCategoryIdxRand(randomCategoryId, size);
         } else if (keyword != null) {
             return searchService.searchVideos(keyword, (pageCount - 1) * size, size);
         }
-        else
+        else {
             return videosService.findAll(PageRequest.of(pageCount - 1, size, Sort.by("idx").descending()));
-    }
-
-    @GetMapping("/videos/rand")
-    public List<VideosResponseDto> findRand(
-            @RequestParam(value = "channel", required = false) String channelId,
-            @RequestParam(value = "category", required = false) Long categoryId,
-            @RequestParam(value = "count", defaultValue = "10") int count
-    ) {
-        if (channelId != null)
-            return videosService.findByChannelIdRand(channelId, count);
-        else if (categoryId != null)
-            return videosService.findByCategoryIdxRand(categoryId, count);
-        else
-            return null;
+        }
     }
 
     @DeleteMapping("/videos/{idx}")
