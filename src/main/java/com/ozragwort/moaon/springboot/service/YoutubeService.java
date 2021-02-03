130a133d0a2c128dffa39f2f6997cdf6bd166279
OzRagwort <ozragwort@gmail.com>
2021-02-03 21:46:36 +0900

    Set time config in update by youtube
    업로드가 3일 이내인 경우 수정일이 10분 이내일 때 갱신
    그외 영상은 360분 이내일 때 갱신

--- src/main/java/com/ozragwort/moaon/springboot/service/YoutubeService.java
+++ src/main/java/com/ozragwort/moaon/springboot/service/YoutubeService.java
@@ -53,7 +53,7 @@ public class YoutubeService {
 
         if (videos != null) {
             ModifiedDurationCheck check = new ModifiedDurationCheck();
-            return check.ModifiedDurationTimeUnder6hour(videos.getModifiedDate()) ?
+            return check.updateTimeCheck(videos.getVideoPublishedDate(), videos.getModifiedDate()) ?
                     videos.getVideoId() : updateVideos(videoListResponse, videos);
         } else {
             return saveVideos(videoListResponse);
