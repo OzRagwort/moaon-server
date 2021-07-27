a4706ae6e9474fb95e2613aa74854d8b85384fb2
OzRagwort <ozragwort@gmail.com>
2021-07-27 22:20:39 +0900

    Add custom toString

--- src/main/java/com/ozragwort/moaon/springboot/dto/categories/CategoriesResponseDto.java
+++ src/main/java/com/ozragwort/moaon/springboot/dto/categories/CategoriesResponseDto.java
@@ -3,6 +3,8 @@ package com.ozragwort.moaon.springboot.dto.categories;
 import com.ozragwort.moaon.springboot.domain.categories.Categories;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 @Getter
 @NoArgsConstructor
@@ -14,6 +16,10 @@ public class CategoriesResponseDto {
     public CategoriesResponseDto(Categories categories) {
         this.idx = categories.getIdx();
         this.categoryName = categories.getCategoryName();
+    }
+
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
     }
 
 }
