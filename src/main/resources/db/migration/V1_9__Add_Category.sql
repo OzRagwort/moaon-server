a311afe0fef1ed0930b6fa800731f5f705e6ea3b
OzRagwort <ozragwort@gmail.com>
2021-08-22 20:58:59 +0900

    Add flyway sql
    Add categories

---
+++ src/main/resources/db/migration/V1_9__Add_Category.sql
@@ -0,0 +1,7 @@
+insert into categories(categories_name) values("pet & aniaml");
+
+insert into categories(categories_name) values("ASMR - KR");
+
+insert into categories(categories_name) values("ASMR - NonKR");
+
+insert into categories(categories_name) values("ASMR - Mukbang");
